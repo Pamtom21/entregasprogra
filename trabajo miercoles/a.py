138fac37e096d9dda9015e49f157c6b5e9459cfd
Pamtom21 <joluis.2.11.04@gmail.com>
2023-10-17 21:52:08 -0300

    a

---
+++ trabajo miercoles/a.py
@@ -0,0 +1,65 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [x for x in arr[1:] if x <= pivot]
+        greater = [x for x in arr[1:] if x > pivot]
+        print(arr)
+        return quick_sort(less) + [pivot] + quick_sort(greater)
+def binary_search(arr, low, high, x):
+    if high >= low:
+ 
+        mid = (high + low) // 2
+ 
+        if arr[mid] == x:
+            print("El numero esta en la posicion: ", mid)
+            return mid
+
+        elif arr[mid] > x:
+            return binary_search(arr, low, mid - 1, x)
+ 
+        else:
+            return binary_search(arr, mid + 1, high, x)
+    else:
+        print("El numero no se encuentra disponible en la lista")
+        return -1
+class sistema:
+    def __init__(self):
+        self.lista= []
+    def agregar(self):
+        d='1'
+        while d == '1': 
+            x=int(input("Ingrese numero: "))
+            self.lista.append(x)
+            print(self.lista)
+            op=input('y para salir o enter para continuar')
+            if op=='y':
+                d= '2'
+    def ordenar(self):
+        print(self.lista)
+        self.lista = quick_sort(self.lista)
+        print(self.lista)
+        return self.lista
+
+    def buscar(self):
+        x=int(input("Ingrese el numero a buscar: "))
+        result=binary_search(self.lista,0, len(self.lista)-1,x)
+        print("El numero buscado esta en la posicion: ", result)
+siste=sistema()
+while True:
+    print("Bienvenido al sistema mas ordenado")
+    print("1. Agregar numeros\n2. Ordenar lista\n3.Buscar numero en la lista\n4.Salir")
+    opcion=input("Ingrese su opcion aqui: ")
+    if opcion=="4":
+        print( "Gracias vuelva pronto")
+        break
+    elif opcion=="1":
+        siste.agregar()
+    elif opcion=="2":
+        siste.ordenar()
+    elif opcion=="3":
+        siste.buscar()
+    else:
+        print("error")
+        
